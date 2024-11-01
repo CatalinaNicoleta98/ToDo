ed2a8b4d3da11faf913257507a24bbd67b87540e
CatalinaNicoleta98 <catvri01@easv365.dk>
2024-11-01 18:40:14 +0100

    Feature for different color based on priority

---
+++ test/PriorityColor.js
@@ -0,0 +1,25 @@
+import { Selector } from 'testcafe';
+
+fixture("ToDo Test")
+    .page("http://localhost:5173/todo/");
+
+    
+
+test('Priority Color', async t => {
+   
+
+    await t
+       //Arrange
+       
+       .typeText(Selector('#task-input'), "High Priority Task")
+       .typeText(Selector('#start-date'), "2024-11-01")
+       .typeText(Selector('#end-date'), "2024-11-03")
+       .click(Selector('#task-priority')) // Click to open dropdown
+       .click(Selector('#priority').withText("High")) // Select the priority option
+        //Act
+       .click(Selector('button').withText('Add Task'))
+      //Assert
+       .expect(Selector('li.task').getStyleProperty('background-color')).eql("rgb(255, 111, 97)");
+
+        
+});
